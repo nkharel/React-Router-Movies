54af139d6f77810a5723be4ab366f1e963a8a82c
Nirajan Kharel <kharelnirajan@gmail.com>
2020-03-18 19:19:14 -0500

    Worked on movie list

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
+import {Route} from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 import SavedList from './Movies/SavedList';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+  const addToSavedList = movie => { 
+    let xx = savedList.map(savedInList => {
+      return savedInList.title
+    })
+    if (xx.includes(movie.title)) {
+      alert('cant add 2 movies of the same title')
+    } else {
+      setSavedList([...savedList, movie])
+    }    
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path='/' component={MovieList} />
+      <Route path='/movies/:id' component={(props) => <Movie {...props} addToSavedList={addToSavedList} list={savedList} />} />
     </div>
   );
 };
